070bfd48caa0f0460714138dd09880b0081b9fc5
bhagwat solunke <bhagwatsolunke007@gmail.com>
2023-04-16 15:40:29 +0530

    All Layout Finished

--- src/components/rightbar/Rightbar.jsx
+++ src/components/rightbar/Rightbar.jsx
@@ -3,7 +3,68 @@ import "./rightbar.css"
 export default function rightbar() {
   return (
     <div className="rightbar">
-      rightbar
+     <div className="rightbarWrapper">
+      <div className="birthdayContainer">
+        <img className="birthdayImg" src="/assets/gift.png" alt="" />
+        <span className="birthdayText">
+          <b>Vikas Kumar </b>and <b>have a birthday today.</b>
+        </span>
+      </div>
+      <img className="rightbarAd" src="/assets/ad.png" alt="" />
+      <h4 className="rightbarTitle">Online Friends</h4>
+      <ul className="rightbarFriendList">
+      <li className="rightbarFriend" >
+        <div className="rightbarProfileImgContainer">
+          <img className="rightbarProfileImg" src="assets/person/2.jpg" alt="" />
+          <span className="rightbarOnline"></span>
+        </div>
+        <span className="rightbarUsername">Shantanu Waghmare</span>
+      </li>
+      <li className="rightbarFriend" >
+        <div className="rightbarProfileImgContainer">
+          <img className="rightbarProfileImg" src="assets/person/2.jpg" alt="" />
+          <span className="rightbarOnline"></span>
+        </div>
+        <span className="rightbarUsername">Shantanu Waghmare</span>
+      </li> <li className="rightbarFriend" >
+        <div className="rightbarProfileImgContainer">
+          <img className="rightbarProfileImg" src="assets/person/2.jpg" alt="" />
+          <span className="rightbarOnline"></span>
+        </div>
+        <span className="rightbarUsername">Shantanu Waghmare</span>
+      </li> <li className="rightbarFriend" >
+        <div className="rightbarProfileImgContainer">
+          <img className="rightbarProfileImg" src="assets/person/2.jpg" alt="" />
+          <span className="rightbarOnline"></span>
+        </div>
+        <span className="rightbarUsername">Shantanu Waghmare</span>
+      </li> <li className="rightbarFriend" >
+        <div className="rightbarProfileImgContainer">
+          <img className="rightbarProfileImg" src="assets/person/2.jpg" alt="" />
+          <span className="rightbarOnline"></span>
+        </div>
+        <span className="rightbarUsername">Shantanu Waghmare</span>
+      </li> <li className="rightbarFriend" >
+        <div className="rightbarProfileImgContainer">
+          <img className="rightbarProfileImg" src="assets/person/2.jpg" alt="" />
+          <span className="rightbarOnline"></span>
+        </div>
+        <span className="rightbarUsername">Shantanu Waghmare</span>
+      </li> <li className="rightbarFriend" >
+        <div className="rightbarProfileImgContainer">
+          <img className="rightbarProfileImg" src="assets/person/2.jpg" alt="" />
+          <span className="rightbarOnline"></span>
+        </div>
+        <span className="rightbarUsername">Shantanu Waghmare</span>
+      </li> <li className="rightbarFriend" >
+        <div className="rightbarProfileImgContainer">
+          <img className="rightbarProfileImg" src="assets/person/2.jpg" alt="" />
+          <span className="rightbarOnline"></span>
+        </div>
+        <span className="rightbarUsername">Shantanu Waghmare</span>
+      </li>
+      </ul>
+     </div>
     </div>
   )
 }
