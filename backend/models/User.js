96ee220cd6220f41c19fd25ff73193bef221abf0
bhagwat solunke <bhagwatsolunke007@gmail.com>
2023-04-23 16:29:20 +0530

    CRUD user & follow , unfollow Api created

--- backend/models/User.js
+++ backend/models/User.js
@@ -33,7 +33,7 @@ const UserSchema = new mongoose.Schema({
         type:Array,
         default:[]
      },
-     following:{
+     followings:{
         type:Array,
         default:[]
      },
@@ -41,8 +41,25 @@ const UserSchema = new mongoose.Schema({
         type: Boolean,
         default:false
 
-
+     },
+     desc:{
+        type:String,
+        max: 50
+     },
+     city:{
+        type:String,
+        max: 50
+     },
+     from :{
+        type : String ,
+        max:50
+     },
+     relationship:{
+        type: Number,
+        enum:[1,2,3],
      }
+     
+
 },
 {
     timestamps:true
