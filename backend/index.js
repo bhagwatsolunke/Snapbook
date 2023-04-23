5f8e35225bcd374eda4d7db959fe2c3fe2f9be70
bhagwat solunke <bhagwatsolunke007@gmail.com>
2023-04-23 17:16:13 +0530

    likes and disklikes api createrd

--- backend/index.js
+++ backend/index.js
@@ -2,11 +2,9 @@ const connectToMongo = require('./db');
 const express = require ("express")
 const helmet = require ("helmet");
 const morgan = require ("morgan");
-
 const userRoute = require("./routes/user")
 const authRoute = require("./routes/auth")
-
-
+const postRoute = require("./routes/posts");
 const app = express()
 const port = 3000
 
@@ -23,7 +21,7 @@ app.use(morgan("combined"));
 
  app.use("/api/users", userRoute);
  app.use("/api/auth", authRoute);
-
+ app.use("/api/posts", postRoute);
 
   app.listen(port, () => {
     console.log(`Socialmedia Backend listening at http://localhost:${port}`);
