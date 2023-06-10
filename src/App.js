6667b0b305d54440b2c4a461594a14e1f6837ea5
bhagwatsolunke <bhagwatsolunke007@gmail.com>
2023-06-10 13:15:28 +0530

    messenger page added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Home from "./pages/home/Home";
 import Profile from "./pages/profile/Profile";
 import Login from "./pages/login/Login";
 import Register from "./pages/register/Register";
+import Messenger from "./pages/messenger/Messenger";
 import {
   BrowserRouter as Router,
   Route,
@@ -24,6 +25,7 @@ function App() {
         <Route exact path="/" element={user ? <Home /> : <Register />} />
         <Route path="/login" element={user ? <Navigate replace to="/" /> : <Login />} />
         <Route path="/register" element={user ? <Navigate replace to="/" /> : <Register />} />
+        <Route path="/messenger" element={!user ? <Navigate replace to="/" /> : <Messenger />} />
         <Route path="/profile/:username" element={<Profile />} />
       </Routes>
     </Router>
---
+++ src/pages/messenger/Messenger.jsx
@@ -0,0 +1,14 @@
+import Topbar from "../../components/topbar/Topbar"
+import "./messenger.css"
+import React from 'react'
+
+export default function Messenger() {
+  return (
+    <>
+    <Topbar/>
+    <div className="messenger">
+     hello
+    </div>
+    </>
+  )
+}
