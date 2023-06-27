eb6aedbf7d6cec61daf95448f5dbd62c347da238
bhagwatsolunke <bhagwatsolunke007@gmail.com>
2023-06-27 19:10:06 +0530

    fetching user conversations

--- src/pages/messenger/Messenger.jsx
+++ src/pages/messenger/Messenger.jsx
@@ -4,8 +4,27 @@ import Topbar from "../../components/topbar/Topbar"
 import "./messenger.css"
 import React from 'react'
 import ChatOnline from "../../components/chatOnline/ChatOnline"
+import { AuthContext } from "../../context/AuthContext"
+import { useContext , useEffect , useState } from "react"
+import axios from "axios";
 
 export default function Messenger() {
+  const [conversations, setConversations] = useState([]);
+
+   const {user} = useContext(AuthContext);
+
+   useEffect(()=>{
+    const getConversations= async ()=>{
+      try{
+      const res = await axios.get("/conversations"+user._id)
+      setConversations(res.data);
+      }catch(err){
+        console.log(err)
+      }
+    }
+    getConversations();
+   },[user._id]);
+
   return (
     <>
     <Topbar/>
@@ -13,11 +32,13 @@ export default function Messenger() {
     <div className="chatMenu">
       <div className="chatMenuWrapper">
         <input placeholder="Search for friends"/>
-        <Conversation/>
-        <Conversation/>
-        <Conversation/>
-        <Conversation/>
-        <Conversation/>
+        { conversations.map((c)=>(
+          <Conversation Conversation={c} currentUser={user} />
+
+        ))
+
+        }
+       
       </div>
     </div>
     <div className="chatBox">
@@ -27,18 +48,7 @@ export default function Messenger() {
         <Message/>
         <Message own={true}/>
         <Message/>
-        <Message own={true}/>
-        <Message/>
-        <Message own={true}/>
-        <Message/>
-        <Message own={true}/>
-        <Message/>
-        <Message own={true}/>
-        <Message/>
-        <Message own={true}/>
-        <Message/>
-        <Message own={true}/>
-        <Message/>
+       
         </div>
         <textarea className="chatMessageInput" placeholder="Write something..."></textarea>
         <button className="chatSubmitButton">send</button>
